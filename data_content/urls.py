576cc9c3c8a7c410c128e3be7e4e2992909e61cc
rony7090 <rony7090@gmail.com>
2023-09-03 14:35:47 +0600

    updated by additional requirement

--- data_content/urls.py
+++ data_content/urls.py
@@ -4,7 +4,9 @@ from .import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("all-stock", views.all_stock, name="allStock"),
     path("edit-stock/<pk>", views.update_stock, name="editStock"),
     path("delete-stock/<pk>", views.delete_stock, name="deleteStock"),
+
 
 ]
